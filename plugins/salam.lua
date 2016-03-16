0e3090bdf1de8f9887345b07ee873dc28b58d67a
Shahab <safaeishahab@yahoo.com>
2016-03-16 15:54:40 +0330

    Update salam.lua

--- plugins/salam.lua
+++ plugins/salam.lua
@@ -2,28 +2,16 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'salam sabr kon bidar sham michatim'
+local text = 'salam bsbr khabam bidar sham michatim'
 if matches[1] == 'salam' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
 end 
-
-function run(msg, matches)
-local reply_id = msg['id']
-local text = 'salam sabr kon bidar sham michatim'
-if matches[1] == 'Salam' then
-    if not is_sudo(msg) then
-reply_msg(reply_id, text, ok_cb, false)
-end
-end 
-end
-end
 end
 return {
 patterns = {
-    "salam",
-    "Salam"
+    "salam"
 },
 run = run
 }
