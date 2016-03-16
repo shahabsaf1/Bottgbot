d8bde70532144196c8188438c83dd987228a880a
Shahab <safaeishahab@yahoo.com>
2016-03-16 15:41:21 +0330

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -3,7 +3,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 local text = 'hi sabr kon sahebam biad michatim'
-if matches[1] == '[Hh]i' then
+if matches[1] == 'hi' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -11,7 +11,7 @@ end
 end
 return {
 patterns = {
-    "[Hh]i"
+    "hi"
 },
 run = run
 }
