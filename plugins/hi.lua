4beff8187a783dc34c5f05c8e7f564cfb7d8f24d
Shahab <safaeishahab@yahoo.com>
2016-03-16 15:45:59 +0330

    Update hi.lua

--- plugins/hi.lua
+++ plugins/hi.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'hi sabr kon sahebam biad michatim'
+local text = 'hi sabr kon online sham michatim'
 if matches[1] == 'hi' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
